d97b490cd6217f4abcfc70482380b98aaffa4364
Andrew Haeffner <andrewhaeffner@outlook.com>
2018-08-03 17:48:00 -0400

    UI improvements & Maze feature additions
    I put all of the buttons into a grid looking stack. I also created the getitem and setitem
    methods for the Maze class. Now is time to move onto a proper maze visualization
    using a canvas. Time to draw.

--- demo-ui.py
+++ demo-ui.py
@@ -1,9 +1,15 @@
 import tkinter as tk
+import tkinter.font as tkFont
+
+
 
 class App:
 	def __init__(self, master):
 		self.frame = tk.Frame(master)
-		self.frame.pack()
+		self.frame.grid()
+
+		helvetica_font = tkFont.Font(root = master, family='Helvetica', size=16, weight='bold')
+		button_width = 18
 
 		self.save_button = tk.Button(self.frame, text='Save to File', command=self.save_maze)
 		self.load_button = tk.Button(self.frame, text='Load from File', command=self.load_maze)
@@ -11,20 +17,45 @@ class App:
 		self.display_button = tk.Button(self.frame, text='Display Maze', command=self.load_maze)
 		self.gen_and_display_button = tk.Button(self.frame, text='Gen & Display Maze', command=self.gen_and_display_maze)
 
-		self.save_button.pack(side=tk.LEFT)
-		self.load_button.pack(side=tk.LEFT)
-		self.generate_button.pack(side=tk.LEFT)
-		self.display_button.pack(side=tk.LEFT)
-		self.gen_and_display_button.pack(side=tk.LEFT)
+		self.save_button.config(height=1, width=button_width) # h and w are in characters.
+		self.load_button.config(height=1, width=button_width)
+		self.generate_button.config(height=1, width=button_width)
+		self.display_button.config(height=1, width=button_width)
+		self.gen_and_display_button.config(height=1, width=button_width)
+
+		self.save_button['font'] = helvetica_font
+		self.load_button['font'] = helvetica_font
+		self.generate_button['font'] = helvetica_font
+		self.display_button['font'] = helvetica_font
+		self.gen_and_display_button['font'] = helvetica_font
 
 
-		self.maze_size_entry = tk.Entry(self.frame)
+		self.save_button.grid(row=1, column=0)
+		self.load_button.grid(row=2, column=0)
+		self.generate_button.grid(row=3, column=0)
+		self.display_button.grid(row=4, column=0)
+		self.gen_and_display_button.grid(row=5, column=0)
+		#self.save_button.pack(side=tk.LEFT)
+		#self.load_button.pack(side=tk.LEFT)
+		#self.generate_button.pack(side=tk.LEFT)
+		#self.display_button.pack(side=tk.LEFT)
+		#self.gen_and_display_button.pack(side=tk.LEFT)
+
+
+		self.maze_size_label = tk.Label(self.frame, font=helvetica_font, text='Maze side length:')
+		self.maze_size_label.config(height=1, width=16)
+
+		self.maze_size_entry = tk.Entry(self.frame, font=helvetica_font)
 		self.maze_size_entry.insert(0, "10")
+
+		self.maze_size_label.grid(row=0, column=0)
+		self.maze_size_entry.grid(row=0, column=1)
+
 		# to get current entry (e)'s text: s = e.get()
 
-		self.maze_size_entry.pack(side=tk.LEFT)
+		#self.maze_size_entry.pack(side=tk.LEFT)
 
-		self.maze = None # represents the Maze that is currently loaded. default = None.
+		self.maze = None # represents the Maze that is currently loaded.
 
 
 
@@ -62,13 +93,30 @@ def gen_maze_no_inner_walls(size):
 
 class Maze:
 	# The maze wasn't meant for you.
-	def __init__(self, size=0, generation_func=None):
+	def __init__(self, size=0, generation_func=gen_maze_no_inner_walls):
 		self.size = size
 		self.gen_func = generation_func # save the func for __repr__
 		# take in a size and a generation func and produce the maze
 		# using generation_func(size).
 
 		self.slabs, self.columns = generation_func(size)
+
+	def __getitem__(self, key):
+		x = key[0]
+		y = key[1]
+		return {'N':self.slabs[y][x], 'S':self.slabs[y+1][x],
+				'W':self.columns[x][y], 'E':self.columns[x+1][y] }
+
+	def __setitem__(self, key, value):
+		x = key[0]
+		y = key[1]
+		letter = key[2]
+
+		if letter == 'N':   self.slabs[y][x] = value
+		elif letter == 'S': self.slabs[y+1][x] = value
+		elif letter == 'W': self.columns[x][y] = value
+		elif letter == 'E': self.columns[x+1][y] = value
+
 	def __str__(self):
 
 		result = []
@@ -103,42 +151,10 @@ class Maze:
 			result.append(''.join(text_row))
 
 		return '\n'.join(result)
-		# It would be convenient to have a console debug tr srepresentation of a maze.
+
 	def __repr__(self):
 		# Provide info on the object.
-
-		result = []
-
-		for i in range(self.size + 1):
-
-			# handle slabs first
-			text_row = []
-			for j in range(self.size+1):
-				text_row.append('+')
-				if j < self.size:
-					if self.slabs[i][j]:
-						symbol = '-'
-					else:
-						symbol = ' '
-					text_row.append(symbol)
-
-			result.append(''.join(text_row))
-
-			# handle columns second
-			text_row = []
-			if i < self.size:
-				for j in range(self.size+1):
-					if self.columns[j][i]:
-						symbol = '|'
-					else:
-						symbol = ' '
-					text_row.append(symbol)
-					if j < self.size:
-						text_row.append(' ')
-
-			result.append(''.join(text_row))
-
-		return 'Maze Object with size ' + str(self.size) + ' and made with ' + str(self.gen_func)
+		return 'Maze Object with side length ' + str(self.size) + ' and made with ' + str(self.gen_func)
 
 def main():
 	root = tk.Tk()
@@ -147,10 +163,14 @@ def main():
 
 def debug():
 	print('Here are some empty mazes for you.')
-	for i in range(10):
+	for i in range(1,10):
 		m = Maze(i, gen_maze_no_inner_walls)
+		print(m)
+		print(m[0,0])
+		m[0, 0, 'S'] = True
+		m[0, 0, 'E'] = True
 		print(m)
 
 if __name__ == '__main__':
-	#main()
-	debug()
+	main()
+	#debug()
